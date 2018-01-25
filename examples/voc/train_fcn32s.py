bba5d2dece30225d9a3dabc3b933cdd0c84b5f01
Matt Barnes <barnes.matt.j@gmail.com>
2018-01-24 20:33:24 -0500

    Back to MSE.

--- examples/voc/train_fcn32s.py
+++ examples/voc/train_fcn32s.py
@@ -18,7 +18,7 @@ configurations = {
     # https://github.com/shelhamer/fcn.berkeleyvision.org
     1: dict(
         max_iteration=100000,
-        lr=1.0e-10,  # -10
+        lr=1.0e-5,  # -10
         momentum=0.99,
         weight_decay=0.0005,
         interval_validate=4000,
@@ -34,9 +34,9 @@ def git_hash():
 
 
 def get_log_dir(model_name, config_id, cfg):
-    name = '000b_{}'.format(datetime.now().strftime('%b%d-%H:%M:%S'))
-    name += '_xe_norm-mse'
-    #name += '_mse_lr1e-6_xavier1e-4-norm'
+    name = '002_{}'.format(datetime.now().strftime('%b%d-%H:%M:%S'))
+    #name += '_xe_norm-mse'
+    name += '_mse_lr1e-5_xavier1e-4-norm'
     name += '_VCS-%s' % git_hash()
     name += '_{}'.format(socket.gethostname().split('.')[0])
 
--- torchfcn/trainer.py
+++ torchfcn/trainer.py
@@ -306,7 +306,7 @@ class Trainer(object):
                 raise ValueError('Cross entropy loss is nan while training')
 
             loss_mse = self.mse_loss(score, target) / len(data)
-            loss = loss_crossentropy
+            loss = loss_mse
 
             print 'Epoch {}. Iteration {}. Training loss {}'.format(self.epoch, self.iteration, loss.data[0])
 
